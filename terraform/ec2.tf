10db83ec511d6064b0021eeeadb31061a11ee878
erkka <erkka.rahikainen@gmail.com>
2023-02-16 18:14:13 +0200

    Create tf resource for ec2

---
+++ terraform/ec2.tf
@@ -0,0 +1,17 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
