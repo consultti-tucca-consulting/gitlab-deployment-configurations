28fafcf76480f8b8080c81786c8d58e2531c485b
erkka <erkka.rahikainen@gmail.com>
2023-02-16 19:04:15 +0200

    Update ec2 type to t3.micro

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,18 +1,8 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-}
-
 resource "aws_instance" "gitlab_ec2_instance" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  ami             = "ami-09e1162c87f73958b"
+  instance_type   = "t3.micro"
   security_groups = [aws_security_group.gitlab_security_group.name]
   tags = {
     Name = "gitlab_instance"
   }
 }
-
